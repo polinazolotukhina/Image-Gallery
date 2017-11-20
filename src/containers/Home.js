5134df483980b03bc2394678460bfc08a1fe9bed
Polina Zolotukhina <polina@iwoork.com>
2017-11-20 16:31:59 +0000

    add Loader

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,12 +3,23 @@ import { connect } from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
 import { browserHistory } from 'react-router';
 import { compose } from 'redux';
-import { firebaseConnect, dataToJS } from 'react-redux-firebase';
+import { firebaseConnect, dataToJS, isLoaded } from 'react-redux-firebase';
 import PropTypes from 'prop-types';
 import Gallery from 'react-grid-gallery';
+import CircularProgress from 'material-ui/CircularProgress';
 
 
 class Home extends Component {
+    checkFiles() {
+        const { uploadedFiles } = this.props;
+
+        if (uploadedFiles) {
+            return (<div style={{ margin: '30px 0' }}>
+                        <Gallery images={Object.values(uploadedFiles)} />
+                    </div>);
+        }
+            return (<h1 className="text-center">Gallery is empty</h1>);
+    }
     render() {
         const { uploadedFiles } = this.props;
         return (
@@ -26,13 +37,11 @@ class Home extends Component {
                     </div>
                 </div>
             {
-                (uploadedFiles)?(
-                <div style={{ margin: '30px 0' }}>
-                    {uploadedFiles &&
-                        <Gallery images={Object.values(uploadedFiles)} />
-                    }
-                </div>
-                ):(<h1 className="text-center">Gallery is empty</h1>)
+                (!isLoaded(uploadedFiles))
+                    ?
+                (<div className="load"> <CircularProgress size={80} thickness={5} /></div>)
+                    :
+                (this.checkFiles())
             }
             </div>
         );
